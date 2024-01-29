3289de28cc5470e061628f2ed5626851e6b023c8
Administrator <administrator@Administrators-Mac-mini.local>
2024-01-30 00:45:25 +0530

    chore: Desktop dashboard design initiated.

--- portfolio/lib/ui/screens/dashboard/desktop_dashboard/dashboard_desktop.dart
+++ portfolio/lib/ui/screens/dashboard/desktop_dashboard/dashboard_desktop.dart
@@ -1,18 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DashboardDesktop extends StatelessWidget {
   const DashboardDesktop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 246, 236, 237),
-      child: const Column(
-        children: [
-          Text(
-            "Desktop View",
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            center: const Alignment(1.0, -0.5),
+            colors: const [
+              Color(0xFF2954A3),
+              Color(0xFF0F103F),
+            ],
+            radius: 1.0.r,
           ),
-        ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    color: Colors.transparent,
+                    height: 50.h,
+                    width: MediaQuery.sizeOf(context).width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Home',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30.w,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'About me',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30.w,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Work',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 30.w,
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Contact',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(
+                  top: MediaQuery.sizeOf(context).height / 5,
+                  bottom: MediaQuery.sizeOf(context).height / 5,
+                ),
+                padding: EdgeInsets.all(20.r),
+                width: 20.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(
+                      50.r,
+                    ),
+                    bottomRight: Radius.circular(
+                      50.r,
+                    ),
+                  ),
+                  color: Colors.green.shade100,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.home,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.person,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.work,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.call,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
