6446f58b3aafc0b40bdd70efa23b516622b15047
Manish1Pandey <manish.pandey@innopay.in>
2024-06-27 11:57:14 +0530

    chore: downgrading flutter and dart version and it's dependencies accordingly.

--- portfolio/macos/Flutter/GeneratedPluginRegistrant.swift
+++ portfolio/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import package_info
 import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTPackageInfoPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
